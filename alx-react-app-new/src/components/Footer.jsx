36cdf6c0eb54a99d9a28e6d6914bd9378635332c
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-08-16 04:33:59 +0000

    added styles

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return(
         <footer>
-            <p>© 2023 City Lovers</p>
+            <p style={{backgroundColor:"purple"}}>© 2023 City Lovers</p>
         </footer>
     )
 }
