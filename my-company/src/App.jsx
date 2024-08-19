72a8ff5d68259c79a04e773594ada94f0c1eae39
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-08-19 00:02:46 +0000

    routes

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,11 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home'; // Import your component
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
