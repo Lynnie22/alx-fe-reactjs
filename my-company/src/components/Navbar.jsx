9dc7abde8fe637a1df79023b7a85d652c88cfef1
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-08-25 21:24:00 +0000

    inline style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
         <nav style={{ backgroundColor: '#e11584', padding: '1em' }}> 
-            <ul >
+            <ul style={{ listStyleType: 'none', margin: 0, padding: 0, display: 'flex', justifyContent: 'space-around' }} >
             <li><Link to="/" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>Home</Link></li>
             <li><Link to="/about" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>About</Link></li>
             <li><Link to="/services" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>Services</Link></li>
