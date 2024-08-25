92daf435685dfd986da2a2d3d794d3e9b9bc3929
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-08-25 21:20:46 +0000

    nav bar

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav style={{ backgroundColor: '#e11584', padding: '1em' }}> 
+            <ul >
+            <li><Link to="/" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>Home</Link></li>
+            <li><Link to="/about" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>About</Link></li>
+            <li><Link to="/services" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>Services</Link></li>
+            <li><Link to="/contact" style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}>Contact</Link></li>
+      </ul>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
