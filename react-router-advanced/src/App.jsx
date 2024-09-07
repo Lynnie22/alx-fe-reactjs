54f2e3199bba144920749333cdf508c2837b1edd
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-09-07 12:34:52 +0000

    used older route method

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,41 +1,66 @@
 
+// import './App.css'
+// import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+// import Profile from './components/Profile';
+// import ProfileSettings from './components/ProfileSettings';
+// import ProfileDetails from './components/ProfileDetails';
+// import BlogPost from './components/BlogPost';
+
+// function App() {
+
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <Profile />,
+//       children: [
+//         {
+//           path: "/details",
+//           element : <ProfileDetails />,
+//         },
+
+//         {
+//           path: "/settings",
+//           element: <ProfileSettings />,
+//         }
+//       ]
+//     },
+//     {
+//       path: "blog/:postId", 
+//       element: <BlogPost />,
+//     },
+//   ]);
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   )
+// }
+
+// export default App
+
+
 import './App.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from './components/Profile';
 import ProfileSettings from './components/ProfileSettings';
 import ProfileDetails from './components/ProfileDetails';
 import BlogPost from './components/BlogPost';
+
 function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Profile />,
-      children: [
-        {
-          path: "/details",
-          element : <ProfileDetails />,
-        },
-
-        {
-          path: "/settings",
-          element: <ProfileSettings />,
-        }
-      ]
-    },
-    {
-      path: "blog/:postId", // Dynamic segment in the URL
-      element: <BlogPost />,
-    },
-  ]);
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Profile page route with nested routes */}
+        <Route path="/" element={<Profile />}>
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+        
+        {/* Dynamic blog post route */}
+        <Route path="blog/:postId" element={<BlogPost />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
