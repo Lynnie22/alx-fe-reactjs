13f6ce48fbb88f3b2e525f3901bae12ba5c41d36
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-09-07 05:53:28 +0000

    changed format

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,20 +1,33 @@
 import { useState } from 'react'
 
 const RegistrationForm = () => {
-    const [formData, setFormData] = useState({
-        username: "",
-        email: "",
-        password:"",
-    })
+    // const [formData, setFormData] = useState({
+    //     username: "",
+    //     email: "",
+    //     password:"",
+    // })
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
     const [err, setErr] = useState("")
 
     const handleChange = (e) => {
         const {name , value} = e.target
-        setFormData({...formData , [name]: value})
+        // setFormData({...formData , [name]: value})
+
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        } else if (name === "username") {
+            setUsername(value);
+        }
     }
 
     const handleSubmit = (e) => {
-        const { email, password, username } = formData;
+        // const { email, password, username } = formData;
         e.preventDefault();
 
         if (!email || !password || !username) {
@@ -23,14 +36,15 @@ const RegistrationForm = () => {
         }
         setErr("");
 
-        console.log("submitted" , formData);
+        // console.log("submitted" , formData);
+
     }
 
   return (
     <form action="" onSubmit={handleSubmit} style={{display:"flex", flexDirection: "column", gap:"1rem"}}>
-        <input type="text" name="username" id="username" value={formData.username} onChange={handleChange} placeholder='Username'/>
-        <input type="email" name='email' id='email' value={formData.email} onChange={handleChange} placeholder='email'/>
-        <input type="password" name='password' id='password' value={formData.password} onChange={handleChange} placeholder='password' />
+        <input type="text" name="username" id="username" value={username} onChange={handleChange} placeholder='Username'/>
+        <input type="email" name='email' id='email' value={email} onChange={handleChange} placeholder='email'/>
+        <input type="password" name='password' id='password' value={password} onChange={handleChange} placeholder='password' />
         <button type='submit'>Submit</button>
         <div style={{color:'red'}}>{err}</div>
     </form>
