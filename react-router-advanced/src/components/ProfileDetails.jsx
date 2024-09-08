66344f43cd446159c707a6f51435996767d731a1
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-09-08 03:38:19 +0000

    protected route

--- react-router-advanced/src/components/ProfileDetails.jsx
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -1,5 +1,11 @@
 const ProfileDetails = () => {
-    return <div><h3>Profile Details</h3></div>;
+    return (
+        <div>
+        <h3>Profile Details</h3>
+        <p>Details about profile ... </p>
+        </div>
+    )
+
   };
 
   export default ProfileDetails
