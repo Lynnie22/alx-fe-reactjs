1717686e2638fe6dd3e3e6642d23aa13fb1b9dd3
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-09-11 23:33:54 +0000

    styles

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,11 @@
+import UserProfile from "./components/UserProfile"
+
 
 function App() {
 
-
   return (
     <>
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <UserProfile/>
     </>
   )
 }
--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -9,10 +9,3 @@ export default {
   },
   plugins: [],
 }
-module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
-  theme: { extend: {} },
-  variants: { extend: {} },
-  plugins: [],
-};
