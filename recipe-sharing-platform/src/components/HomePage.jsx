4632fa6d83d91345cdc4c6777afad9390a0bc8a6
Marilyn Adjei <adjeimarilyn22@gmail.com>
2024-09-15 20:23:49 +0000

    hover

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -22,9 +22,9 @@ const HomePage = () => {
   return (
 <div className="container mx-auto p-4 ">
       <h1 className="text-3xl font-bold mb-6">Recipe List</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
         {recipes.map(recipe => (
-          <div key={recipe.id} className="bg-white rounded-lg shadow-md overflow-hidden">
+          <div key={recipe.id} className="bg-white rounded-lg shadow-md overflow-hidden hover:translate-y-4">
             <img
               src={recipe.image}
               alt={recipe.title}
